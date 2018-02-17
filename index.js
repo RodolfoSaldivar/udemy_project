b1e9ba4b0264b159cffd92b8287289f0bd4bcd16
Rodolfo Saldivar <rodolfo.saldivar@udem.edu>
2018-02-17 16:14:08 -0600

    agregarme en el arreglo

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi:'there'});
+	res.send([{hi:'there'}, {soy:'Rodolfo'}]);
 });
+
+// https://git.heroku.com/pacific-stream-45589.git
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
